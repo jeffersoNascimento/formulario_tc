a2450a66e9bf8b5598b2b4ae4da2f2680679763e
JeffersoNascimento <jeffersonn561@gmail.com>
2023-10-06 10:54:40 -0300

    Tornando o site responsivo

--- script.js
+++ script.js
@@ -1,3 +1,87 @@
+// Função para ajustar o estilo do conteúdo para dispositivos móveis
+function ajustarParaDispositivoMovel() {
+  const header = document.querySelector('header');
+  const text1 = document.querySelector('.text1');
+  const text2 = document.querySelector('.text2');
+  const link = document.querySelector('.link');
+
+  if (window.innerWidth <= 576) { // Verifica se a largura da tela é menor ou igual a 576 pixels (celulares)
+      header.style.height = '200px'; // Reduz a altura do header
+      text1.style.textAlign = 'center'; // Centraliza horizontalmente o texto
+      text2.style.textAlign = 'center'; // Centraliza horizontalmente o texto
+      text1.style.display = 'flex'; // Usa flexbox para centralizar verticalmente o texto
+      text1.style.alignItems = 'center'; // Centraliza verticalmente o texto
+      text2.style.display = 'flex'; // Usa flexbox para centralizar verticalmente o texto
+      text2.style.alignItems = 'center'; // Centraliza verticalmente o texto
+      link.style.fontSize = '16px'; // Ajusta o tamanho do texto do link
+  } else {
+      header.style.height = '400px'; // Restaura a altura original do header
+      text1.style.textAlign = 'left'; // Retorna o alinhamento do texto ao padrão
+      text2.style.textAlign = 'left'; // Retorna o alinhamento do texto ao padrão
+      text1.style.display = 'block'; // Remove flexbox
+      text2.style.display = 'block'; // Remove flexbox
+      link.style.fontSize = 'inherit'; // Retorna o tamanho do texto ao padrão
+  }
+}
+
+// Chama a função quando a página é carregada e quando a janela é redimensionada
+window.addEventListener('load', ajustarParaDispositivoMovel);
+window.addEventListener('resize', ajustarParaDispositivoMovel);
+
+
+// Chama a função quando a página é carregada e quando a janela é redimensionada
+window.addEventListener('load', ajustarParaDispositivoMovel);
+window.addEventListener('resize', ajustarParaDispositivoMovel);
+
+
+// Chama a função quando a página é carregada e quando a janela é redimensionada
+window.addEventListener('load', ajustarParaDispositivoMovel);
+window.addEventListener('resize', ajustarParaDispositivoMovel);
+  
+  // Adicione o evento de clique para cada botão de orçamento
+  const botoesOrcamento = document.querySelectorAll('.button');
+  botoesOrcamento.forEach(botao => {
+  botao.addEventListener('click', function () {
+    window.location.href = "index.html";
+  });
+});
+
+  const botoesOrcamento1 = document.querySelectorAll('.button1');
+  botoesOrcamento1.forEach(botao => {
+  botao.addEventListener('click', function () {
+    window.location.href = "index.html";
+  });
+});
+
+const botoesOrcamento2 = document.querySelectorAll('.button2');
+  botoesOrcamento2.forEach(botao => {
+  botao.addEventListener('click', function () {
+    window.location.href = "index.html";
+  });
+});
+
+const botoesOrcamento3 = document.querySelectorAll('.button3');
+  botoesOrcamento3.forEach(botao => {
+  botao.addEventListener('click', function () {
+    window.location.href = "index.html";
+  });
+});
+
+const botoesOrcamento4 = document.querySelectorAll('.button4');
+  botoesOrcamento4.forEach(botao => {
+  botao.addEventListener('click', function () {
+    window.location.href = "index.html";
+  });
+});
+
+
+  function aumentarProduto(catalog) {
+    catalog.style.transform = 'scale(1.05)'; /* Aumentando em 5% */
+}
+
+function restaurarProduto(catalog) {
+    catalog.style.transform = 'scale(1)'; /* Restaurando a escala original */
+}
 
 // Função para animar os elementos na entrada da página
 function animatePageEntry() {
@@ -5,6 +89,7 @@ function animatePageEntry() {
   const cards = document.querySelectorAll('.card-item');
   const textElements = document.querySelectorAll('.text');
   const textElements1 = document.querySelectorAll('.text1');
+  const textElements2 = document.querySelectorAll('.text2');
 
   // Animar o cabeçalho (header)
   header.style.opacity = '0';
@@ -34,6 +119,15 @@ function animatePageEntry() {
       }, 800 + textElements.length * 200 + index * 200); // Atraso maior para cada elemento de texto adicional
   });
 
+  textElements2.forEach((textElement2, index) => {
+    textElement2.style.opacity = '0';
+    textElement2.style.transform = 'translateY(30px)';
+    setTimeout(() => {
+        textElement2.style.opacity = '1';
+        textElement2.style.transform = 'translateY(0)';
+    }, 800 + textElements.length * 200 + index * 200); // Atraso maior para cada elemento de texto adicional
+});
+
   // Animar os cards
   cards.forEach((card, index) => {
       card.style.opacity = '0';
@@ -58,11 +152,3 @@ function runLightAnimation() {
 }
 
 window.addEventListener('load', runLightAnimation);
-
-function voltar() {
-  // URL externo a ser aberto em uma nova guia
-  var urlExterno = 'https://www.canva.com/design/DAFq4mzO5s0/tdSb389x8zfQM0YiLd2u_A/view?website#2';
-
-  // Abrir o link externo em uma nova guia
-  window.open(urlExterno, '_blank');
-}
